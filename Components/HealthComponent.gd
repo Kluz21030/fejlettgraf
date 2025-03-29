4c303a695afc00e2d8276fc50af122ed93d8d3af
Kluz21030 <gateshades21030@gmail.com>
2025-03-29 23:53:37 +0100

    feat: started hit testing with dummy

--- Components/HealthComponent.gd
+++ Components/HealthComponent.gd
@@ -1,12 +1,14 @@
 class_name HealthComponent extends Node
 
 signal died
-
+@export var timer: Timer
 @export var MAX_HEALTH: int
 var health: int:
 	set(value):
 		health = clampi(value, 0, MAX_HEALTH)
+		print(health)
 		if health == 0:
+			owner.queue_free()
 			died.emit()
 
 func _ready() -> void:
@@ -14,3 +16,8 @@ func _ready() -> void:
 
 func take_damage(damage: int):
 	health -= damage
+	var kecske: MeshInstance3D = owner
+	kecske.mesh.material.albedo_color = Color.RED
+	timer.start(0.5)
+	await timer.timeout
+	kecske.mesh.material.albedo_color = Color.WHITE
--- Components/Weapon.gd
+++ Components/Weapon.gd
@@ -1,10 +1,16 @@
-class_name Weapon extends StaticBody3D
+class_name Weapon extends Area3D
 
 @export var weapon_name: StringName
-@export var hitbox: CollisionShape3D
+@export var base_damage: int
+@export var two_handed: bool
 
 func _ready() -> void:
 	if not weapon_name:
 		weapon_name = name
-	if not hitbox:
-		hitbox = find_children("*", "CollisionShape3D")[0]
+	area_entered.connect(_on_hit)
+
+func _on_hit(area: Area3D) -> void:
+	if area is HurtboxComponet:
+		var attack: Attack = Attack.new()
+		attack.damage = base_damage
+		area.damage(attack)
