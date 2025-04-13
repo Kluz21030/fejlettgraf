c430ecd39819e662d6bf1505b434a1cc11d0ebde
Kluz21030 <gateshades21030@gmail.com>
2025-04-13 19:49:31 +0200

    feat: added unarmed skeleton minion enemy

--- Enemies/States/idle.gd
+++ Enemies/States/idle.gd
@@ -1,14 +1,14 @@
 extends State
 
-@export var frame_counter: int = 0
+var frame_counter: int = 0
+
 @export var in_range_check_frequency: int = 20
 @export var min_idle_time: float = .5
 @export var max_idle_time: float = 2.0
-
-@onready var idle_timer: Timer = $IdleTimer
+@export var idle_timer: Timer
 
 func enter(previous_state_path: String, data: Dictionary = {}) -> void:
-	animation_player.play("enemy_animations/Idle_Combat", .3 if previous_state_path == "Attack" else -1)
+	animation_player.play(animation, .3 if previous_state_path == "Attack" else -1)
 	if body.player_in_range:
 		idle_timer.timeout.connect(_on_idle_timeout)
 		idle_timer.start(randf_range(min_idle_time, max_idle_time))
