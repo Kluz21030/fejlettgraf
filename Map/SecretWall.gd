5147a58a64fe1163d07c6d7c0f30c3b420349b16
Kluz21030 <gateshades21030@gmail.com>
2025-05-12 12:26:21 +0200

    feat: secret wall tween is now created on ready

--- Map/SecretWall.gd
+++ Map/SecretWall.gd
@@ -3,7 +3,9 @@ class_name SecretWall extends Area3D
 @export var mesh: MeshInstance3D
 @export var wall_root: Node3D
 
+var tween: Tween
+
 func destroy() -> void:
-	var tween: Tween = create_tween().set_ease(Tween.EASE_IN)
+	tween = create_tween().set_ease(Tween.EASE_IN)
 	tween.tween_property(mesh, "transparency", 1.0, 0.5)
 	tween.tween_callback(wall_root.queue_free)
